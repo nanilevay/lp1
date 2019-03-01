d55f9bd5af286ca55b8861132938d6a63c692b8f
nanilev <dianuski99@gmail.com>
2019-03-01 10:16:03 +0000

    Show Bye World!

---
+++ OlaMundo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bye World!");
+            Console.ReadKey();
+        }
+    }
+}
