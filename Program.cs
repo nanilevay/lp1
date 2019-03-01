51c67c14413d11473d78053bee8dc5850629e7d9
Diana Levay <45174677+nanilevay@users.noreply.github.com>
2019-03-01 12:20:02 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            for (int i = 0; i < args.Length ; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+        }
+    }
+}
